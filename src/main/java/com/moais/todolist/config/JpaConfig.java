ff9541a57a70415b2710bb838037649e72ceba27
derulo taser <iih907@naver.com>
2023-09-16 21:04:47 +0900

    Jpa Config 설정 파일 추가

---
+++ src/main/java/com/moais/todolist/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.moais.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
